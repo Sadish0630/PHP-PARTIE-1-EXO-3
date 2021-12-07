5016277ec3146c34290ff23f2a0c291f4b31d8af
audemar <audemarsadish@gmail.com>
2021-12-07 13:38:30 +0100

    exo3 terminé

---
+++ exo3.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css"/>
+    <title>PARTIE 1 EXO 3</title>
+</head>
+<body>
+    <p>
+   <?php
+   $km = '1';
+   echo $km ;
+   ?>
+   </p>
+   <p>
+   <?php
+    $km = '1';
+   echo $km += '2' ;
+   ?>
+   </p>
+   <p>
+   <?php
+   $km = '1';
+   echo $km += '124' ;
+   ?>
+   </p>
+</body>
+</html>
